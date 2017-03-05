fd94aabba60c91fa37ae7240d0a364cce7f4d47f
oubaidallah <ayadioubaidallah@gmail.com>
2017-03-05 19:09:48 +0100

    Read Image DPI

--- src/main/java/recognizer/ImageDPI.java
+++ src/main/java/recognizer/ImageDPI.java
@@ -1,61 +1,35 @@
 package main.java.recognizer;
 
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.util.Iterator;
-import javax.imageio.ImageIO;
-import javax.imageio.ImageReader;
-import javax.imageio.metadata.IIOMetadata;
-import javax.imageio.metadata.IIOMetadataFormatImpl;
-import javax.imageio.metadata.IIOMetadataNode;
-import javax.imageio.stream.ImageInputStream;
-import javax.xml.soap.Node;
 
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.NodeList;
+import org.apache.sanselan.ImageInfo;
+import org.apache.sanselan.ImageReadException;
+import org.apache.sanselan.Sanselan;
 
 public class ImageDPI {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		
-		//BufferedImage iis = ImageIO.read(new File("test-dpi.jpg"));
-		ImageInputStream iis = ImageIO.createImageInputStream(new File("test-dpi.jpg"));
-	    Iterator it = ImageIO.getImageReaders(iis);
-	    if (!it.hasNext())
-	    {
-	        System.err.println("No reader for this format");
-	        return;
-	    }
-	    ImageReader reader = (ImageReader) it.next();
-	    reader.setInput(iis);
+		File file = new File("C:\\Sanstitre.png");
+		
+		ImageInfo imageInfo = null;
+		try {
+			imageInfo = Sanselan.getImageInfo(file);
+		} catch (ImageReadException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-	    IIOMetadata meta = reader.getImageMetadata(0);
-	    IIOMetadataNode root = (IIOMetadataNode) meta.getAsTree("javax_imageio_1.0");
-	    NodeList nodes = root.getElementsByTagName("HorizontalPixelSize");
-	    if (nodes.getLength() > 0)
-	    {
-	        IIOMetadataNode dpcWidth = (IIOMetadataNode) nodes.item(0);
-	        NamedNodeMap nnm = dpcWidth.getAttributes();
-	        Node item = (Node) nnm.item(0);
-	        int xDPI = Math.round(25.4f / Float.parseFloat(item.getNodeValue()));
-	        System.out.println("xDPI: " + xDPI);
-	    }
-	    else
-	        System.out.println("xDPI: -");
-	    if (nodes.getLength() > 0)
-	    {
-	        nodes = root.getElementsByTagName("VerticalPixelSize");
-	        IIOMetadataNode dpcHeight = (IIOMetadataNode) nodes.item(0);
-	        NamedNodeMap nnm = dpcHeight.getAttributes();
-	        Node item = (Node) nnm.item(0);
-	        int yDPI = Math.round(25.4f / Float.parseFloat(item.getNodeValue()));
-	        System.out.println("yDPI: " + yDPI);
-	    }
-	    else
-	        System.out.println("yDPI: -");
+		final int physicalWidthDpi = imageInfo.getPhysicalWidthDpi();
+		final int physicalHeightDpi = imageInfo.getPhysicalHeightDpi();
+		
+		
+		System.out.println(physicalWidthDpi +" ... " + physicalHeightDpi);
+
 	}
 
 }
